cd5cd227b5d301908e62d978504a0f0c4717cee3
Ruben Vitt <dev@rubeen.one>
2022-06-23 15:16:26 +0200

    add git- & docker-config

---
+++ init-symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# create Symlinks for config FOLDERS
+ln -s ~/.dotfiles/lima ~/.lima
+ln -s ~/.dotfiles/docker ~/.docker
+
+# create Symlinks for FILES
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig

d07d6d5aa7d10442d65e7561cbfe373bfd124344
Ruben Vitt <dev@rubeen.one>
2022-08-10 08:11:56 +0200

    change dock-add script

--- mac/dock-add.sh
+++ mac/dock-add.sh
@@ -10,21 +10,25 @@ fi
 
 __dock_item() {
     printf '%s%s%s%s%s' \
-        '<dict><key>tile-data</key><dict><key>file-
-  data</key><dict>' \
-        '<key>_CFURLString</key><string>' \
-        "$1" \
-        '</string><key>_CFURLStringType</key><integer>0</integer>' \
-        '</dict></dict></dict>'
+           '<dict><key>tile-data</key><dict><key>file-data</key><dict>' \
+           '<key>_CFURLString</key><string>' \
+           "$1" \
+           '</string><key>_CFURLStringType</key><integer>0</integer>' \
+           '</dict></dict></dict>'
 }
 
 printf '%s' 'Setting up Dock icons...'
-defaults write com.apple.dock
+# clear items
+# defaults write com.apple.dock persistent-apps -array
+
+# for dockItem in {/System/Applications/{"Mail","Notes","System Preferences","App Store","Preview"},/Applications/{"iTerm","Visual Studio Code"}}.app; do
+#   defaults write com.apple.dock persistent-apps -array-add "$(__dock_item ${dockItem})"
+# done
 
 # persistent-apps -array "$(__dock_item /Applications/Safari.app)" \
 #     "$(__dock_item /System/Applications/Utilities/Terminal.app)"
 
-persistent-apps -array-add "$(__dock_item /Applications/${1}.app)" \
-    
+defaults write com.apple.dock persistent-apps -array-add "$(__dock_item /Applications/$1.app)"
+
 killall Dock
 printf '%s\n' ' done.'

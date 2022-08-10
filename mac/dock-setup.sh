fd79c7c503b86270579ce6bd6fccfe1839ee7fb1
Ruben Vitt <dev@rubeen.one>
2022-08-10 07:59:27 +0200

    add docker & script for dock setup

---
+++ mac/dock-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+#requrire one argument "appname"
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 'appname'"
+  exit 1
+fi
+
+__dock_item() {
+    printf '%s%s%s%s%s' \
+        '<dict><key>tile-data</key><dict><key>file-
+  data</key><dict>' \
+        '<key>_CFURLString</key><string>' \
+        "$1" \
+        '</string><key>_CFURLStringType</key><integer>0</integer>' \
+        '</dict></dict></dict>'
+}
+
+printf '%s' 'Setting up Dock icons...'
+defaults write com.apple.dock
+
+# persistent-apps -array "$(__dock_item /Applications/Safari.app)" \
+#     "$(__dock_item /System/Applications/Utilities/Terminal.app)"
+
+persistent-apps -array-add "$(__dock_item /Applications/${1}.app)" \
+    
+killall Dock
+printf '%s\n' ' done.'

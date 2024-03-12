9525d749c2c0c95ada651be8a8fa237866997b48
Ruben Vitt <dev@rubeen.dev>
2024-03-12 16:08:30 +0100

    update dotfiles

--- config/fish/functions/fish_greeting.fish
+++
@@ -1,5 +0,0 @@
-function fish_greeting
-  set_color $fish_color_autosuggestion
-echo "🐟 | 🐀🐀🐈🐕🐈🐀🐀 |🐟"
-  set_color normal
-end
--- config/fish/functions/fzf.fish
+++
@@ -1,1 +0,0 @@
-alias fzf-i "fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
